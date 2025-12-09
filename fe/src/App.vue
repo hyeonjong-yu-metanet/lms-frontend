89ddc568f850893b603faf95a5c1a477384c35eb
hyeonjong-yu-metanet <hj0511@metanetglobal.com>
2025-12-09 17:34:59 +0900

    라우터 설정

--- fe/src/App.vue
+++ fe/src/App.vue
@@ -1,11 +1,14 @@
-<script setup></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <nav>
+    <router-link to="/">Home</router-link>
+    <span> | </span>
+  </nav>
+
+  <router-view />
 </template>
 
-<style scoped></style>
+<style scoped>
+nav {
+  margin-bottom: 16px;
+}
+</style>
---
+++ fe/src/views/Home.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <h1>Welcome</h1>
+    <p>This is the home page. Use the navigation to open the Leaflet practice page.</p>
+  </div>
+</template>
+
+<style scoped></style>
